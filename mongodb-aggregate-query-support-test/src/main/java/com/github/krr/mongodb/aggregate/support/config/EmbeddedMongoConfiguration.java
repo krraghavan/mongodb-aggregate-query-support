616113951039b29355c43a39fac5098cea124111
Kollivakkam Raghavan <raghavankr@gmail.com>
2024-01-07 18:00:20 -0800

    Make a temp dir for downloading the distribution each time to ensure that parallel downloads don't clobber each other

--- mongodb-aggregate-query-support-test/src/main/java/com/github/krr/mongodb/aggregate/support/config/EmbeddedMongoConfiguration.java
+++ mongodb-aggregate-query-support-test/src/main/java/com/github/krr/mongodb/aggregate/support/config/EmbeddedMongoConfiguration.java
@@ -2,11 +2,16 @@ package com.github.krr.mongodb.aggregate.support.config;
 
 import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.mongo.transitions.ImmutableMongod;
 import de.flapdoodle.embed.mongo.transitions.Mongod;
 import de.flapdoodle.embed.mongo.transitions.RunningMongodProcess;
 import de.flapdoodle.embed.process.io.ProcessOutput;
 import de.flapdoodle.embed.process.io.Processors;
 import de.flapdoodle.embed.process.io.StreamProcessor;
+import de.flapdoodle.embed.process.io.directories.PersistentDir;
+import de.flapdoodle.embed.process.store.DownloadCache;
+import de.flapdoodle.embed.process.store.LocalDownloadCache;
+import de.flapdoodle.reverse.transitions.Derive;
 import de.flapdoodle.reverse.transitions.Start;
 import de.flapdoodle.types.Try;
 import org.slf4j.Logger;
@@ -20,7 +25,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.UUID;
 
 @Configuration
 public class EmbeddedMongoConfiguration {
@@ -46,31 +53,38 @@ public class EmbeddedMongoConfiguration {
     String runningMongoVersion = runningVersion.numericVersion().asString();
     System.out.println("Tests will use Mongo version:" + runningMongoVersion);
     LOGGER.info("Starting MongoDb process (version:{}) on port {}", runningMongoVersion, mongoDbPort);
-
-    Mongod mongod = Mongod.builder()
-                          .net(Start.to(Net.class).initializedWith(Net.defaults()
-                                                                      .withPort(mongoDbPort)))
-                          .processOutput(Start.to(ProcessOutput.class)
-                                              .providedBy(Try.<ProcessOutput,
-                                                                 IOException>supplier(() ->
-                                                                                          ProcessOutput.builder()
-                                                                                                       .output(
-                                                                                                           Processors.named(
-                                                                                                               "[mongod>]",
-                                                                                                               new FileStreamProcessor(
-                                                                                                                   new File(
-                                                                                                                       "target/mongod.log"))))
-                                                                                                       .error(
-                                                                                                           new FileStreamProcessor(
-                                                                                                               new File(
-                                                                                                                   "target/mongod-error.log")))
-                                                                                                       .commands(
-                                                                                                           Processors.namedConsole(
-                                                                                                               "[console>]"))
-                                                                                                       .build())
-                                                             .mapToUncheckedException(RuntimeException::new))
-                                              .withTransitionLabel("create named console"))
-                          .build();
+    ImmutableMongod.Builder mongodBuilder = Mongod.builder();
+    // to support parallel executions, we may have multiple downloads in parallel.  In order to
+    // avoid clobbering in progress downloads we download to a diff directory each time.  Do this
+    // only if not on Mac so that local builds don't download unnecessarily.
+    Path basedir = new File(System.getProperty("java.io.tmpdir"), UUID.randomUUID().toString()).toPath();
+    mongodBuilder.downloadCache(Derive.given(PersistentDir.class)
+                                      .state(DownloadCache.class)
+                                      .deriveBy(storeBaseDir -> new LocalDownloadCache(basedir))
+                                      .withTransitionLabel("downloadCache"));
+    Mongod mongod = mongodBuilder.net(Start.to(Net.class).initializedWith(Net.defaults().withPort(mongoDbPort)))
+                                 .processOutput(Start.to(ProcessOutput.class)
+                                                     .providedBy(Try.<ProcessOutput,
+                                                                        IOException>supplier(() ->
+                                                                                                 ProcessOutput.builder()
+                                                                                                              .output(
+                                                                                                                  Processors.named(
+                                                                                                                      "[mongod>]",
+                                                                                                                      new FileStreamProcessor(
+                                                                                                                          new File(
+                                                                                                                              "target/mongod.log"))))
+                                                                                                              .error(
+                                                                                                                  new FileStreamProcessor(
+                                                                                                                      new File(
+                                                                                                                          "target/mongod-error.log")))
+                                                                                                              .commands(
+                                                                                                                  Processors.namedConsole(
+                                                                                                                      "[console>]"))
+                                                                                                              .build())
+                                                                    .mapToUncheckedException(
+                                                                        RuntimeException::new))
+                                                     .withTransitionLabel("create named console"))
+                                 .build();
     mongodProcess = mongod.start(runningVersion).current();
   }
 
@@ -78,7 +92,7 @@ public class EmbeddedMongoConfiguration {
     Version[] versions = Version.values();
     // fragile - counting on convention in this ennm
     Version latest = versions[versions.length - 2];
-    if(mongoVersion == null) {
+    if (mongoVersion == null) {
       return getLatestMongoVersion("null", latest);
     }
     return Arrays.stream(Version.values()).filter(version -> version.numericVersion().asString().equals(mongoVersion))
